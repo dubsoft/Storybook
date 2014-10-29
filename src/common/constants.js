0bb8df7db6bbf20d3ed5335340bcbd6821123b7d
wlaurance <w.laurance@gmail.com>
2014-10-29 12:49:49 -0400

    https endpoint

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,7 +3,7 @@ angular.module("sf.constants", [])
   .constant("appName", "Story Book")
   .constant("tagLine", "Learn by Writing Together")
   .constant("baseFbUrl", "https://storybook.firebaseio.com/")
-  .constant("empiricalBaseURL", "http://staging.quill.org/api/v1")
+  .constant("empiricalBaseURL", "https://staging.quill.org/api/v1")
 
 ;
 
